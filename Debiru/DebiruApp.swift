cdd684612573922b78ce815462eaf936dc196ea4
mbpolan <mbpolan@gmail.com>
2021-03-13 20:29:01 -0500

    Initial Commit

---
+++ Debiru/DebiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebiruApp.swift
+//  Debiru
+//
+//  Created by Mike Polan on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct DebiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
