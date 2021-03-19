089a334a8f86daae578b2e3cc51dfac85b379dc9
mbpolan <mbpolan@gmail.com>
2021-03-18 21:04:05 -0400

    Standard font styles for comments

--- Debiru/UI/CatalogView.swift
+++ Debiru/UI/CatalogView.swift
@@ -5,6 +5,7 @@
 //  Created by Mike Polan on 3/15/21.
 //
 
+import AppKit
 import Combine
 import SwiftUI
 
@@ -33,7 +34,7 @@ struct CatalogView: View {
                     Text(thread.subject ?? "")
                         .font(.title)
                     
-                    Text(thread.content ?? "")
+                    RichTextView(thread.content ?? "")
                 }
                 Spacer()
             }
@@ -50,6 +51,45 @@ struct CatalogView: View {
                 viewModel.pendingThreads = nil
             }
         }
+    }
+}
+
+struct RichTextView: NSViewRepresentable {
+    
+    private let html: String
+    
+    init(_ html: String) {
+        self.html = html
+    }
+    
+    func makeNSView(context: Context) -> NSTextField {
+        let view = NSTextField()
+        view.isEditable = false
+        view.isBezeled = false
+        view.isSelectable = true
+        view.drawsBackground = false
+        view.textColor = NSColor.textColor
+        
+        DispatchQueue.main.async {
+            let data = Data(self.html.utf8)
+            
+            if let rendered = try? NSMutableAttributedString(
+                data: data,
+                options: [.documentType: NSAttributedString.DocumentType.html],
+                documentAttributes: nil) {
+                
+                let range = NSMakeRange(0, rendered.length)
+                let font = NSFont.preferredFont(forTextStyle: .body, options: [:])
+                rendered.addAttribute(.font, value: font, range: range)
+                rendered.addAttribute(.foregroundColor, value: NSColor.textColor, range: range)
+                view.attributedStringValue = rendered
+            }
+        }
+        
+        return view
+    }
+    
+    func updateNSView(_ nsView: NSTextField, context: Context) {
     }
 }
 
