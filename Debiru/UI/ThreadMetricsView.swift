a2c9b079da3ea72238f231f15770eb86c7959d83
mbpolan <mbpolan@gmail.com>
2021-04-16 19:54:53 -0400

    Fix ugly text wrapping in thread metrics

--- Debiru/UI/ThreadMetricsView.swift
+++ Debiru/UI/ThreadMetricsView.swift
@@ -59,6 +59,7 @@ struct ThreadMetricsView: View {
             return Group {
                 Image(systemName: icon)
                 makeNumberText(value)
+                    .lineLimit(1)
             }
             .toErasedView()
         } else {
